78e2f7ce15dd99f4a85c6cf1bb7ce977d050a49a
AdimLukman <32480217+AdimLukman@users.noreply.github.com>
2018-01-03 22:42:47 +0800

    Deklarasi Variabel

---
+++ tugas.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+	// DEKLARASI VARIABEL
+	double a[3][5]={{4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2}};
+		
+	double w[5]={5,3,4,4,2};
+	double ha1,ha2,ha3;
+	
+	// SETTING PRESISI DESIMAL
+	cout.setf(ios::fixed);cout.setf(ios::showpoint);cout.precision(4);
+			
+	// MENAMPILKAN NILAI
+	cout << "MATRIKS X :"<<endl;
+	for(int i=0;i<3;i++){
+		for(int j=0;j<5;j++){
+			cout<< a[i][j] <<" | ";
+		}
+		cout<<endl;
+	}
