2a816cecc1e2ab86c493789366e72d045f508e8d
AdimLukman <32480217+AdimLukman@users.noreply.github.com>
2018-01-03 22:44:46 +0800

    Penampilan Hasil

--- tugas.cpp
+++ tugas.cpp
@@ -41,4 +41,32 @@ int main(){
 		cout<<endl;
 	}
 	cout<<endl;
+	// MENAMPILKAN NILAI W
+	cout << "BOBOT W :"<<endl;
+	for(int i=0;i<5;i++){
+		cout<< w[i] <<" | ";
+	}
+	cout << endl;
+	cout << endl;
+	
+	//MENAMPILKAN HASIL R*W
+	cout << "MATRIKS R * W :"<<endl;
+		double RWa[3][5];
+	for(int i=0;i<3;i++){
+		for(int j=0;j<5;j++){
+			RWa[i][j]=Ra[i][j]*w[j];
+			cout<< RWa[i][j]<<" | ";
+		}
+		cout<<endl;
+	}
+	cout << endl;
+	
+	//Hasil Akhir
+	cout << "HASIL AKHIR  :"<<endl;
+	for(int i=0;i<3;i++){
+		cout<<"Hasil "<<i+1<<" = "<<RWa[i][0]+RWa[i][1]+RWa[i][2]+RWa[i][3]+RWa[i][4]<<endl;	
+	}
+	cout<<endl;
+}
+
 	
